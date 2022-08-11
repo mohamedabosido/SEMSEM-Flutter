2ba12af1e588bc0f174fe4ddd46e39aae06a3f82
mohamedabosido <abosido138011@gmail.com>
2022-08-12 00:19:23 +0300

    Profile Picture Crop

--- lib/widgets/profile_picture_widget.dart
+++ lib/widgets/profile_picture_widget.dart
@@ -1,13 +1,17 @@
+import 'dart:io';
+
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_profile_picture/flutter_profile_picture.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:shimmer/shimmer.dart';
 import 'package:tokoto/constant/constant.dart';
 import 'package:tokoto/helpers/fb_storage_controller.dart';
 import 'package:tokoto/prefs/user_preferences_controller.dart';
+import 'package:tokoto/widgets/app_button.dart';
 
 final profilePhotoProvider =
     FutureProvider.family<String, String>((ref, imageName) async {
@@ -16,16 +20,21 @@ final profilePhotoProvider =
       .getDownloadURL();
 });
 
-class ProfilePictureWidget extends ConsumerWidget {
+class ProfilePictureWidget extends ConsumerStatefulWidget {
   const ProfilePictureWidget({
     Key? key,
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState createState() => _ProfilePictureWidgetState();
+}
+
+class _ProfilePictureWidgetState extends ConsumerState<ProfilePictureWidget> {
+  @override
+  Widget build(BuildContext context) {
     final storage = FbStorage();
     final profilePic = ref.watch(profilePhotoProvider(
-        'User${UserPreferencesController().user.id}-ProfilePicture'));
+        'User-${UserPreferencesController().user.id}-ProfilePicture'));
     return Stack(
       children: [
         profilePic.when(
@@ -59,16 +68,65 @@ class ProfilePictureWidget extends ConsumerWidget {
           bottom: 0,
           child: GestureDetector(
             onTap: () async {
-              final ImagePicker picker = ImagePicker();
-              final XFile? photo =
-                  await picker.pickImage(source: ImageSource.gallery);
-              storage
-                  .uploadImage(
-                      path: photo!.path,
-                      fileName:
-                          'User${UserPreferencesController().user.id}-ProfilePicture')
-                  .then((value) => ref.refresh(profilePhotoProvider(
-                      'User${UserPreferencesController().user.id}-ProfilePicture')));
+              showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                  backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+                  title: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      IconButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          icon: const Icon(Icons.close)),
+                      const Text('Choose an Option'),
+                    ],
+                  ),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  content: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      AppButton(
+                          text: 'Gallery',
+                          onPressed: () {
+                            pickImage(ImageSource.gallery)
+                                .whenComplete(() => cropImage())
+                                .then((value) => {
+                                      storage
+                                          .uploadImage(
+                                              path: croppedFile!.path,
+                                              fileName:
+                                                  'User-${UserPreferencesController().user.id}-ProfilePicture')
+                                          .then((value) => ref.refresh(
+                                              profilePhotoProvider(
+                                                  'User-${UserPreferencesController().user.id}-ProfilePicture')))
+                                    });
+                            Navigator.pop(context);
+                          }),
+                      const SizedBox(height: kDefaultPadding / 2),
+                      AppButton(
+                          text: 'Camera',
+                          onPressed: () {
+                            pickImage(ImageSource.camera)
+                                .whenComplete(() => cropImage())
+                                .then((value) => {
+                                      storage
+                                          .uploadImage(
+                                              path: croppedFile!.path,
+                                              fileName:
+                                                  'User-${UserPreferencesController().user.id}-ProfilePicture')
+                                          .then((value) => ref.refresh(
+                                              profilePhotoProvider(
+                                                  'User-${UserPreferencesController().user.id}-ProfilePicture')))
+                                    });
+                            Navigator.pop(context);
+                          }),
+                    ],
+                  ),
+                ),
+              );
             },
             child: CircleAvatar(
               radius: 23,
@@ -90,5 +148,44 @@ class ProfilePictureWidget extends ConsumerWidget {
         )
       ],
     );
+  }
+
+  File? pickedFile;
+  File? croppedFile;
+
+  Future pickImage(ImageSource source) async {
+    final ImagePicker picker = ImagePicker();
+    var photo = await picker.pickImage(source: source);
+    try {
+      if (photo != null) {
+        var kFile = File(photo.path);
+        setState(() {
+          pickedFile = kFile;
+        });
+      }
+    } catch (e) {
+      return;
+    }
+  }
+
+  Future cropImage() async {
+    CroppedFile? file = await ImageCropper().cropImage(
+      sourcePath: pickedFile!.path,
+      aspectRatioPresets: [
+        CropAspectRatioPreset.square,
+        CropAspectRatioPreset.ratio3x2,
+        CropAspectRatioPreset.original,
+        CropAspectRatioPreset.ratio4x3,
+        CropAspectRatioPreset.ratio16x9
+      ],
+      uiSettings: [
+        IOSUiSettings(title: 'Cropper'),
+      ],
+    );
+    if (file != null) {
+      setState(() {
+        croppedFile = File(file.path);
+      });
+    }
   }
 }
